29ae1247168cebd61d8f9ecd505a4e38ceafb9b9
Richard diZerega <richdizz@outlook.com>
2015-08-24 19:46:50 -0500

    Added mvc project

---
+++ ASP.NET MVC/Completed/GraphExplorerMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GraphExplorerMVC.MvcApplication" Language="C#" %>
---
+++ ASP.NET MVC/Completed/GraphExplorerMVC/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphExplorerMVC.Models
+{
+    public class UserModel
+    {
+        public string displayName { get; set; }
+        public Guid objectId { get; set; }
+    }
+}
